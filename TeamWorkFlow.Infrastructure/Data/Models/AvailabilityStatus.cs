9922308c525f1f45fbfaadc3dcee806f7b235f34
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-02 17:38:30 +0200

    AvailabilityStatus class added

---
+++ TeamWorkFlow.Infrastructure/Data/Models/AvailabilityStatus.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using TeamWorkFlow.Infrastructure.Constants;
+
+namespace TeamWorkFlow.Infrastructure.Data.Models
+{
+    [Comment("Operator availability status")]
+    public class AvailabilityStatus
+    {
+        [Key]
+        [Comment("Operator identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.AvailabilityStatusNameMaxLength)]
+        [Comment("Availability status name")]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Operator> Operators { get; set; } = new List<Operator>();
+    }
+}
