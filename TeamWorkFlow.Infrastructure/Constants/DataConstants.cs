e41b6c582d5173b683f666168b96920dafa4fc37
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-02 17:39:28 +0200

    DataConstants class created

---
+++ TeamWorkFlow.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace TeamWorkFlow.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        /// <summary>
+        /// AvailabilityStatus DataConstants
+        /// </summary>
+        public const int AvailabilityStatusNameMinLength = 5;
+        public const int AvailabilityStatusNameMaxLength = 15;
+
+        /// <summary>
+        /// Operator DataConstants
+        /// </summary>
+        
+        public const int OperatorFullNameMinLength = 2;
+        public const int OperatorFullNameMaxLength = 100;
+
+        public const int OperatorEmailMinLength = 5;
+        public const int OperatorEmailMaxLength = 100;
+
+        public const int OperatorPhoneMinLength = 5;
+        public const int OperatorPhoneMaxLength = 20;
+    }
+}
