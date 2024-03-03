518a71be7320c0f257af533229f52a0315ad6b34
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-03 18:04:13 +0200

    not used using in the header removed

--- TeamWorkFlow.Infrastructure/Data/Models/Part.cs
+++ TeamWorkFlow.Infrastructure/Data/Models/Part.cs
@@ -1,7 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic.CompilerServices;
 using TeamWorkFlow.Infrastructure.Constants;
 
 namespace TeamWorkFlow.Infrastructure.Data.Models
