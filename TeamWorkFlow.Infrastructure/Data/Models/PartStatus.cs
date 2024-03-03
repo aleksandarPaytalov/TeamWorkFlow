6d58f11a448f8decd6b8d327363e4a0aae2ef5cd
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-03 16:50:16 +0200

    PartStatus db model created

---
+++ TeamWorkFlow.Infrastructure/Data/Models/PartStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using TeamWorkFlow.Infrastructure.Constants;
+
+namespace TeamWorkFlow.Infrastructure.Data.Models
+{
+    [Comment("Part status Db model")]
+    public class PartStatus
+    {
+        [Key]
+        [Comment("PartStatus identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.PartStatusNameMaxLength)]
+        [Comment("PartStatus name")]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Part> Parts { get; set; } = new List<Part>();
+    }
+}
