9bd95ddea9e2ad8fec822411b5b810cd9094c810
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-03 16:18:43 +0200

    TaskOperator and TaskStatus data model created

---
+++ TeamWorkFlow.Infrastructure/Data/Models/TaskStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeamWorkFlow.Infrastructure.Data.Models
+{
+    [Comment("TaskStatus data model")]
+    public class TaskStatus
+    {
+        [Key]
+        [Comment("TaskStatus identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("TaskStatus name")]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
