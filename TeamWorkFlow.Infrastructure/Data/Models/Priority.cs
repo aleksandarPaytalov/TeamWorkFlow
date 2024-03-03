a5ca65e3d61031ceeeab84ae08b31b8febfe5b61
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-03 16:17:59 +0200

    Priority data model for the Task created

---
+++ TeamWorkFlow.Infrastructure/Data/Models/Priority.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using TeamWorkFlow.Infrastructure.Constants;
+
+namespace TeamWorkFlow.Infrastructure.Data.Models
+{
+    [Comment("Priority data model")]
+    public class Priority
+    {
+        [Key]
+        [Comment("Priority identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.PriorityNameMaxLength)]
+        [Comment("Priority name")]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
