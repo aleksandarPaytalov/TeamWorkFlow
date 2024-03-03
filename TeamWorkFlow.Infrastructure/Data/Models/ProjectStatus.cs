4bf2ec6a7e5297a25f2e5c0d4398b59a5feab1d7
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-03 18:03:17 +0200

    ProjectStatus model added

---
+++ TeamWorkFlow.Infrastructure/Data/Models/ProjectStatus.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using TeamWorkFlow.Infrastructure.Constants;
+
+namespace TeamWorkFlow.Infrastructure.Data.Models
+{
+    [Comment("ProjectStatus data model")]
+    public class ProjectStatus
+    {
+        [Key]
+        [Comment("ProjectStatus identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.ProjectStatusNameMaxLength)]
+        [Comment("ProjectStatus name")]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Project> Tasks { get; set; } = new List<Project>();
+    }
+}
