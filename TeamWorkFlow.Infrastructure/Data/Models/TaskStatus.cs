441789cac653d4b42d0013ef206afd121a01a089
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-04 16:40:46 +0200

    validation for Name added

--- TeamWorkFlow.Infrastructure/Data/Models/TaskStatus.cs
+++ TeamWorkFlow.Infrastructure/Data/Models/TaskStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using TeamWorkFlow.Infrastructure.Constants;
 
 namespace TeamWorkFlow.Infrastructure.Data.Models
 {
@@ -11,6 +12,7 @@ namespace TeamWorkFlow.Infrastructure.Data.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(DataConstants.TaskStatusNameMaxLength)]
         [Comment("TaskStatus name")]
         public string Name { get; set; } = string.Empty;
 
