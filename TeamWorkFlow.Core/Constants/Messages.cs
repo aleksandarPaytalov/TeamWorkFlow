e3df5aaa113035f8eec8d46b30c487a16456e1ce
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-12 21:39:42 +0200

    error message for name length have been added

--- TeamWorkFlow.Core/Constants/Messages.cs
+++ TeamWorkFlow.Core/Constants/Messages.cs
@@ -3,5 +3,7 @@
     public static class Messages
     {
         public const string DateFormat = "dd/MM/yyyy";
-    }
+
+        public const string StringLength = "The field {0}, must be between {1} and {2} characters long.";
+	}
 }
