b47164df8c59e7c18954a03bb9aaac4059e0459a
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-12 22:21:34 +0200

    AddTaskViewModel created

--- TeamWorkFlow.Core/Models/Task/AddTaskViewModel.cs
+++ TeamWorkFlow.Core/Models/Task/AddTaskViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace TeamWorkFlow.Core.Models.Task
+﻿using System.ComponentModel.DataAnnotations;
+using TeamWorkFlow.Core.Constants;
+using TeamWorkFlow.Infrastructure.Constants;
+
+namespace TeamWorkFlow.Core.Models.Task
 {
     public class AddTaskViewModel
     {
-    }
+		[Required]
+	    public int Id { get; set; }
+
+		[Required]
+		[StringLength(DataConstants.ProjectNumberMaxLength,
+			MinimumLength = DataConstants.ProjectNumberMinLength,
+			ErrorMessage = Messages.StringLength)]
+	    public string ProjectNumber { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(DataConstants.TaskNameMaxLength,
+			MinimumLength = DataConstants.TaskNameMinLength,
+			ErrorMessage = Messages.StringLength)]
+		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(DataConstants.TaskDescriptionMaxLength,
+			MinimumLength = DataConstants.TaskDescriptionMinLength,
+			ErrorMessage = Messages.StringLength)]
+		public string Description { get; set; } = string.Empty;
+		
+	    public string? Deadline { get; set; }
+
+		[Required]
+	    public string StartDate { get; set; } = string.Empty;
+
+	    public string? EndDate { get; set; }
+
+	    public int PriorityId { get; set; }
+	    public ICollection<PriorityViewModel> Priorities { get; set; } = new List<PriorityViewModel>();
+
+	    public int StatusId { get; set; }
+	    public ICollection<StatusViewModel> Statuses { get; set; } = new List<StatusViewModel>();
+	}
 }
