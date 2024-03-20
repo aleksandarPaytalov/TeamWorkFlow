42da5eb324199f509ac730306c034faca45f20c9
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-20 21:19:01 +0200

    Availability view model and Operator view model created

---
+++ TeamWorkFlow.Core/Models/Operator/AvailabilityStatusViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TeamWorkFlow.Core.Constants;
+using TeamWorkFlow.Infrastructure.Constants;
+
+namespace TeamWorkFlow.Core.Models.Operator
+{
+	public class AvailabilityStatusViewModel
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(DataConstants.AvailabilityStatusNameMaxLength,
+			MinimumLength = DataConstants.AvailabilityStatusNameMinLength,
+			ErrorMessage = Messages.StringLength)]
+		public string Name { get; set; } = string.Empty;
+	}
+}
---
+++ TeamWorkFlow.Core/Models/Operator/OperatorViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TeamWorkFlow.Core.Models.Operator
+{
+	public class OperatorViewModel
+	{
+		public int Id { get; set; }
+
+		public string FullName { get; set; } = string.Empty;
+
+		public string AvailabilityStatus { get; set; } = string.Empty;
+
+		public string Email { get; set; } = null!;
+
+		public string PhoneNumber { get; set; } = string.Empty;
+
+		public bool IsActive { get; set; }
+
+		public int Capacity { get; set; }
+	}
+}
