0d4fbe542f8b806ba5119827bf8203373c9eb600
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-30 18:21:39 +0200

    Part models for All method created. Enumeration for sorting added

---
+++ TeamWorkFlow.Core/Models/Part/AllPartsQueryModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TeamWorkFlow.Core.Enumerations;
+
+namespace TeamWorkFlow.Core.Models.Part
+{
+    public class AllPartsQueryModel
+    {
+        public int PartsPerPage { get; } = 6;
+
+        public string Status { get; set; } = string.Empty;
+
+        [Display(Name = "Search by part name")]
+        public string Search { get; set; } = string.Empty;
+
+        public PartSorting Sorting { get; init; }
+        
+        public int CurrentPage { get; set; } = 1;
+
+        public int TotalPartsCount { get; set; }
+
+        public IEnumerable<string> Statuses { get; set; } = null!;
+
+        public IEnumerable<PartServiceModel> Parts { get; set; } = new List<PartServiceModel>();
+    }
+}
