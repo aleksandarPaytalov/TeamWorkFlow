405497c9f72fbdda829a59db4f2088d6a2eac556
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-30 18:22:48 +0200

    AllAsync method implemented in the PartService

--- TeamWorkFlow.Core/Services/PartService.cs
+++ TeamWorkFlow.Core/Services/PartService.cs
@@ -1,15 +1,100 @@
-﻿using TeamWorkFlow.Core.Contracts;
-using TeamWorkFlow.Infrastructure.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TeamWorkFlow.Core.Contracts;
+using TeamWorkFlow.Core.Enumerations;
+using TeamWorkFlow.Core.Models.Part;
+using TeamWorkFlow.Infrastructure.Common;
+using TeamWorkFlow.Infrastructure.Data.Models;
 
 namespace TeamWorkFlow.Core.Services
 {
-	public class PartService : IPartService
-	{
-		private readonly TeamWorkFlowDbContext _context;
+    public class PartService : IPartService
+    {
+        private readonly IRepository _repository;
 
-		public PartService(TeamWorkFlowDbContext context)
-		{
-			_context = context;
-		}
-	}
+        public PartService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public async Task<PartQueryServiceModel> AllAsync(
+            int partsPerPage = 1, 
+            int currentPage = 1, 
+            PartSorting sorting = PartSorting.LastAdded,
+            string? search = null, 
+            string? status = null)
+        {
+            var partsToBeDisplayed = _repository.AllReadOnly<Part>();
+
+            if (status != null)
+            {
+                partsToBeDisplayed = partsToBeDisplayed
+                    .Where(p => p.PartStatus.Name == status);
+            }
+
+            if (search != null)
+            {
+                string normalizePartName = search.ToLower();
+
+                if (int.TryParse(normalizePartName, out int toolNumber))
+                {
+                    partsToBeDisplayed = partsToBeDisplayed
+                        .Where(p => p.ToolNumber == toolNumber);
+                }
+
+                else
+                {
+                    partsToBeDisplayed = partsToBeDisplayed
+                        .Where(p => p.Name.ToLower().Contains(normalizePartName) ||
+                                    p.PartArticleNumber.ToLower().Contains(normalizePartName) ||
+                                    p.PartClientNumber.ToLower().Contains(normalizePartName));
+                }
+            }
+
+            partsToBeDisplayed = sorting switch
+            {
+                PartSorting.ProjectNumberDescending => partsToBeDisplayed
+                    .OrderByDescending(p => p.Project.ProjectNumber),
+                PartSorting.ProjectNumberAscending => partsToBeDisplayed
+                    .OrderBy(p => p.Project.ProjectNumber),
+                _ => partsToBeDisplayed
+                    .OrderByDescending(p => p.Id)
+            };
+
+            var parts = await partsToBeDisplayed
+                .Skip((currentPage - 1) * partsPerPage)
+                .Take(partsPerPage)
+                .ProjectToPartServiceModels()
+                .ToListAsync();
+
+            int totalParts = await partsToBeDisplayed.CountAsync();
+
+            return new PartQueryServiceModel()
+            {
+                Parts = parts,
+                TotalPartsCount = totalParts
+            };
+        }
+
+        public async  Task<IEnumerable<string>> AllStatusNamesAsync()
+        {
+            return await _repository.AllReadOnly<PartStatus>()
+                .Select(ps => ps.Name)
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<PartStatusServiceModel>> AllStatusesAsync()
+        {
+            return await _repository.AllReadOnly<PartStatus>()
+                .Select(ps => new PartStatusServiceModel()
+                {
+                    Id = ps.Id,
+                    Name = ps.Name
+                })
+                .ToListAsync();
+        }
+
+        
+    }
 }
