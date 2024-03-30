92519cdfcfbc15ce6139c0acbd4e611793ebe894
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-30 18:24:28 +0200

    All page for Part that will show all parts with pagination and sorting finished

--- TeamWorkFlow.Core/Contracts/IPartService.cs
+++ TeamWorkFlow.Core/Contracts/IPartService.cs
@@ -1,7 +1,20 @@
-﻿namespace TeamWorkFlow.Core.Contracts
+﻿using TeamWorkFlow.Core.Enumerations;
+using TeamWorkFlow.Core.Models.Part;
+
+namespace TeamWorkFlow.Core.Contracts
 {
 	public interface IPartService
-	{
+    {
+        Task<PartQueryServiceModel> AllAsync(
+            int partsPerPage = 1,
+            int currentPage = 1,
+            PartSorting sorting = PartSorting.LastAdded,
+            string? searchByName = null,
+            string? status = null
+            );
 
-	}
+        Task<IEnumerable<string>> AllStatusNamesAsync();
+
+        Task<IEnumerable<PartStatusServiceModel>> AllStatusesAsync();
+    }
 }
