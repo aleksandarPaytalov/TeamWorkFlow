a1797c07e1a1ec5ccd275c99a12094bde906a9a2
aleksandarPaytalov <apaytalov89@gmail.com>
2024-04-13 20:58:46 +0300

    using sorted and removed

--- TeamWorkFlow.Infrastructure/Data/SeedDatabase/OperatorConfig.cs
+++ TeamWorkFlow.Infrastructure/Data/SeedDatabase/OperatorConfig.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TeamWorkFlow.Infrastructure.Data.Models;
 
 namespace TeamWorkFlow.Infrastructure.Data.SeedDatabase
 {
-	internal class OperatorConfig : IEntityTypeConfiguration<Operator>
+    internal class OperatorConfig : IEntityTypeConfiguration<Operator>
 	{
 		public void Configure(EntityTypeBuilder<Operator> builder)
 		{
