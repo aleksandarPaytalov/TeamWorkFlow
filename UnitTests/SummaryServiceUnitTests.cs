5de630ae492f3420a70f8efe671b770f5361ce16
aleksandarPaytalov <apaytalov89@gmail.com>
2024-04-20 18:48:36 +0300

    SummaryService unit tests added

--- UnitTests/SummaryServiceUnitTests.cs
+++ UnitTests/SummaryServiceUnitTests.cs
@@ -8,7 +8,6 @@ using TeamWorkFlow.Infrastructure.Common;
 using TeamWorkFlow.Infrastructure.Data;
 using TeamWorkFlow.Infrastructure.Data.Models;
 using Task = System.Threading.Tasks.Task;
-using TaskStatus = System.Threading.Tasks.TaskStatus;
 
 namespace UnitTests
 {
@@ -52,7 +51,52 @@ namespace UnitTests
 		[Test]
 		public async Task SummaryAsync_ShouldReturnCorrectSummary()
 		{
-			
+			// Arrange
+			var totalTasksCount = await _repository.AllReadOnly<TeamWorkFlow.Infrastructure.Data.Models.Task>()
+				.CountAsync();
+			var finishedTasksCount = await _repository.AllReadOnly<TeamWorkFlow.Infrastructure.Data.Models.Task>()
+				.Where(t => t.TaskStatus.Name.ToLower() == "finished")
+				.CountAsync();
+
+			int totalProjects = await _repository.AllReadOnly<Project>()
+				.CountAsync();
+			int projectsInProduction = await _repository.AllReadOnly<Project>()
+				.Where(p => p.ProjectStatus.Name.ToLower() == "in production")
+				.CountAsync();
+
+			int totalParts = await _repository.AllReadOnly<Part>()
+				.CountAsync();
+			int totalPartsWithStatusApproved = await _repository.AllReadOnly<Part>()
+				.Where(p => p.PartStatus.Name.ToLower() == "released")
+				.CountAsync();
+
+			int totalWorkers = await _repository.AllReadOnly<Operator>()
+				.Where(w => w.IsActive)
+				.CountAsync();
+			int totalAvailableWorkers = await _repository.AllReadOnly<Operator>()
+				.Where(w => w.AvailabilityStatus.Name.ToLower() == "at work" && w.IsActive)
+				.CountAsync();
+
+			int totalMachines = await _repository.AllReadOnly<Machine>()
+				.CountAsync();
+			int totalAvailableMachines = await _repository.AllReadOnly<Machine>()
+				.Where(m => m.IsCalibrated == true)
+				.CountAsync();
+
+			// Act
+			var result = await _summaryService.SummaryAsync();
+
+			// Assert
+			Assert.That(result.FinishedTasks, Is.EqualTo(finishedTasksCount));
+			Assert.That(result.TotalTasks, Is.EqualTo(totalTasksCount));
+			Assert.That(result.TotalProjects, Is.EqualTo(totalProjects));
+			Assert.That(result.ProjectsInProduction, Is.EqualTo(projectsInProduction));
+			Assert.That(result.TotalParts, Is.EqualTo(totalParts));
+			Assert.That(result.TotalApprovedParts, Is.EqualTo(totalPartsWithStatusApproved));
+			Assert.That(result.TotalWorkers, Is.EqualTo(totalWorkers));
+			Assert.That(result.TotalAvailableWorkers, Is.EqualTo(totalAvailableWorkers));
+			Assert.That(result.TotalMachines, Is.EqualTo(totalMachines));
+			Assert.That(result.TotalAvailableMachines, Is.EqualTo(totalAvailableMachines));
 		}
 
 
