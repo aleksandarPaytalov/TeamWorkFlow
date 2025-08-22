13d20e9233da9079dbec25636f837c3ee6bea13f
aleksandarPaytalov <apaytalov89@gmail.com>
2025-08-22 16:27:30 +0300

    error hadlers pages implemented

--- TeamWorkFlow/Controllers/HomeController.cs
+++ TeamWorkFlow/Controllers/HomeController.cs
@@ -34,22 +34,72 @@ namespace TeamWorkFlow.Controllers
 		{
 			if (statusCode.HasValue)
 			{
-				if (statusCode == 401)
+				switch (statusCode.Value)
 				{
-					return View("Error401");
-				}
-				else if (statusCode == 404)
-				{
-					ViewBag.ErrorMessage = "404 Page not found Exception";
-					return View("Error404");
-				}
-				else if (statusCode == 500)
-				{
-					return View("Error500");
+					case 400:
+						ViewBag.ErrorCode = "400";
+						ViewBag.ErrorTitle = "Bad Request";
+						ViewBag.ErrorMessage = "The request could not be understood by the server due to malformed syntax.";
+						ViewBag.ErrorDescription = "Please check your request and try again.";
+						return View("Error400");
+
+					case 401:
+						ViewBag.ErrorCode = "401";
+						ViewBag.ErrorTitle = "Unauthorized";
+						ViewBag.ErrorMessage = "You are not authorized to access this resource.";
+						ViewBag.ErrorDescription = "Please log in with appropriate credentials.";
+						return View("Error401");
+
+					case 403:
+						ViewBag.ErrorCode = "403";
+						ViewBag.ErrorTitle = "Forbidden";
+						ViewBag.ErrorMessage = "You don't have permission to access this resource.";
+						ViewBag.ErrorDescription = "Contact your administrator if you believe this is an error.";
+						return View("Error403");
+
+					case 404:
+						ViewBag.ErrorCode = "404";
+						ViewBag.ErrorTitle = "Page Not Found";
+						ViewBag.ErrorMessage = "The page you are looking for could not be found.";
+						ViewBag.ErrorDescription = "The page may have been moved, deleted, or you entered the wrong URL.";
+						return View("Error404");
+
+					case 408:
+						ViewBag.ErrorCode = "408";
+						ViewBag.ErrorTitle = "Request Timeout";
+						ViewBag.ErrorMessage = "The server timed out waiting for the request.";
+						ViewBag.ErrorDescription = "Please try again. If the problem persists, contact support.";
+						return View("Error408");
+
+					case 429:
+						ViewBag.ErrorCode = "429";
+						ViewBag.ErrorTitle = "Too Many Requests";
+						ViewBag.ErrorMessage = "You have sent too many requests in a short period.";
+						ViewBag.ErrorDescription = "Please wait a moment before trying again.";
+						return View("Error429");
+
+					case 500:
+						ViewBag.ErrorCode = "500";
+						ViewBag.ErrorTitle = "Internal Server Error";
+						ViewBag.ErrorMessage = "An internal server error occurred.";
+						ViewBag.ErrorDescription = "We are working to fix the problem. Please try again later.";
+						return View("Error500");
+
+					default:
+						ViewBag.ErrorCode = statusCode.ToString();
+						ViewBag.ErrorTitle = "Error";
+						ViewBag.ErrorMessage = "An unexpected error occurred.";
+						ViewBag.ErrorDescription = "Please try again or contact support if the problem persists.";
+						return View("ErrorGeneric");
 				}
 			}
 
-			return View(); 
+			// Default error view for cases without status code
+			ViewBag.ErrorCode = "Unknown";
+			ViewBag.ErrorTitle = "Error";
+			ViewBag.ErrorMessage = "An error occurred while processing your request.";
+			ViewBag.ErrorDescription = "Please try again or contact support if the problem persists.";
+			return View();
 		}
 	}
 }
